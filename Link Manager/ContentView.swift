d8cd5397cbd0ff230543c2a24a37892eee5d1a20
Hiren Lakhtariya <hirenlakhatariya04465@gmail.com>
2026-01-09 05:45:47 +0530

    Initial Commit

---
+++ Link Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Link Manager
+//
+//  Created by Hiren Lakhatariya on 09/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Link Manager/Link_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Link_ManagerApp.swift
+//  Link Manager
+//
+//  Created by Hiren Lakhatariya on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Link_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
